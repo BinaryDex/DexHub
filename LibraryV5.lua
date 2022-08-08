538f628eaec56afeff6c4306282f3fb021c0fed8
BinaryDex <82985536+BinaryDex@users.noreply.github.com>
2022-08-08 19:40:15 -0400

    Update LibraryV5.lua

--- LibraryV5.lua
+++ LibraryV5.lua
@@ -118,7 +118,7 @@ end
 function Library:change_theme(toTheme)
 	Library.CurrentTheme = toTheme
 	local c = self:lighten(toTheme.Tertiary, 20)
-	Library.DisplayName.Text = "Welcome, <font color='rgb("..math.floor(c.R*255)..","..math.floor(c.G*255)..","..math.floor(c.B*255)..")'> <b>"..LocalPlayer.DisplayName.."</b> </font>"
+	Library.DisplayName.Text = "Welcome,<font color='rgb("..math.floor(c.R*255)..","..math.floor(c.G*255)..","..math.floor(c.B*255)..")'><b>"..LocalPlayer.DisplayName.."</b></font>"
 	for color, objects in next, Library.ThemeObjects do
 		local themeColor = Library.CurrentTheme[color]
 		for _, obj in next, objects do
